b33789dff0a854f0adf65796eada140f5803c6f5
sanyam43 <sanyam4392@gmail.com>
2015-10-16 01:59:04 +0530

    user bl dal  etc (get)

--- ProjectManagement/ProjectManagement.SPHelper/ProjManagementAdmin.cs
+++ ProjectManagement/ProjectManagement.SPHelper/ProjManagementAdmin.cs
@@ -15,6 +15,7 @@ namespace ProjectManagement.SPHelper
 
         #region Stored Procedure Constants
         private const string PROC_GETALLPROJECTS = "dbo.GetAllProjects";
+        private const string PROC_GETALLUSERS = "dbo.GetAllUsers";
 
         #endregion
 
@@ -28,6 +29,15 @@ namespace ProjectManagement.SPHelper
         private const string PARAM_PROJECT_NAME = "@project_name";
         private const string PARAM_PROJECT_CODE= "@project_code";
         private const string PARAM_PROJECT_LEAD_NAME = "@project_lead";
+        private const string PARAM_USER_CHANGEDBY = "@changed_by";
+        private const string PARAM_USER_CREATEDDATE = "@changed_by";
+        private const string PARAM_USER_CHANGEDDATE = "@changed_date";
+
+        private const string PARAM_USER_NAME = "@user_name";
+        private const string PARAM_USER_ID = "@user_id";
+        private const string PARAM_USER_EMAIL = "@user_email";
+        private const string PARAM_ROLE_ID = "@role_id";
+        private const string PARAM_PASSWORD = "@password";
 
 
         public static SqlDataReader GetAllProjects(out int retValue)
@@ -36,6 +46,15 @@ namespace ProjectManagement.SPHelper
             SqlDataReader dr = null;
             SqlParameter[] parms = GetGetAllProjectsParams();
             dr = ExecuteReader(PROC_GETALLPROJECTS, parms, out retValue);
+
+            return dr;
+        }
+        public static SqlDataReader GetAllUsers(out int retValue)
+        {
+            retValue = -1;
+            SqlDataReader dr = null;
+            SqlParameter[] parms = GetAllUsersParams();
+            dr = ExecuteReader(PROC_GETALLUSERS, parms, out retValue);
 
             return dr;
         }
@@ -53,6 +72,26 @@ namespace ProjectManagement.SPHelper
 
                 sqlParms[0].Direction = ParameterDirection.ReturnValue;
                 SQLHelper.CacheParameters(PROC_GETALLPROJECTS, sqlParms);
+            }
+
+            //Assigning values to parameter
+            sqlParms[0].Value = -1;
+            return sqlParms;
+        }
+        
+        private static SqlParameter[] GetAllUsersParams()
+        {
+            SqlParameter[] sqlParms = SQLHelper.GetCachedParameters(PROC_GETALLUSERS);
+            if (sqlParms == null)
+            {
+                sqlParms = new SqlParameter[]
+                            {
+                                new SqlParameter(PARAM_RETURN, SqlDbType.Int)
+                               
+                            };
+
+                sqlParms[0].Direction = ParameterDirection.ReturnValue;
+                SQLHelper.CacheParameters(PROC_GETALLUSERS, sqlParms);
             }
 
             //Assigning values to parameter
